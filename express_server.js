25e7e96790f00dcec25ed025b2ed51385a952140
kcruz95 <kyle.cruz@hotmail.ca>
2021-06-07 23:32:15 -0700

    express_server.js

--- express_server.js
+++ express_server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+app.set("view engine", "ejs");
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -20,7 +22,15 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/hello", (req, res) => {
+  const templateVars = { greeting: 'Hello World!' };
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+  res.render("hello_world", templateVars);
+// <!-- This would display the string "Hello World!" -->
+// <h1><%= greeting %></h1>
+// <!-- This line will only show if greeting is truthy -->
+  // <h1><%= greeting %></h1>
+  // <% }%>
+// By using the <%= %> syntax, we tell EJS that we want the result of this code to show up on our page. If we would like to run some code without it displaying on the page, then we can use the slightly different syntax of <% %>
 });
 
 app.get("/set", (req, res) => {
@@ -31,3 +41,13 @@ app.get("/set", (req, res) => {
  app.get("/fetch", (req, res) => {
   res.send(`a = ${a}`);
  });
+
+ app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: "http://localhost:8080/urls/b2xVn2" };
+  res.render("urls_show", templateVars);
+});
